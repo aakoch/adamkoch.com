621e6dd70dcf199a32656dbdbdbb87eb6fc93227
Adam Koch <adam@dogtired.org>
2021-08-07 19:48:11 -0500

    state of flux

---
+++ src/components/card.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+div
+  i(v-bind:class='icon')
+  .card(style='width: 18rem;')
+    .card-body
+      h5.card-title {{ cardTitle }}
+      p.card-text {{ excerpt }}
+      a.btn.btn-primary(href='#') {{ buttonText }}
+</template>
+
+<style lang="scss" scoped>
+$primary: #406275;
+$dark: #43464A;
+$light: #DEE3E6;
+.fas {
+  color: $primary;
+  background-color: #f5f7f8;
+  font-size: 3em;
+  // left: 50%;
+  text-align: center;
+  // transform: translateX(-50%);
+  transform: translateY(20px);
+
+  border: red solid 2px;
+  padding: .5em;
+  width: 4em;
+  border-radius: 10px 10px 0 0;
+  border-bottom: 0;
+  margin-left: 40px;
+}
+.card {
+  border-top: 0;
+  border-radius: 10px;
+  border: red solid 2px;
+}
+</style>
+
+<script>
+export default {
+  props: ['cardTitle', 'excerpt', 'icon', 'buttonText']
+}
+</script>
---
+++ src/landing.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.center-row.container-fluid.g-0#main
+  .row
+    .col
+      .container
+        .row
+          .col(v-for="card in cards")
+            card(:key='card.id' :cardTitle='card.title' :excerpt='card.excerpt' :icon='card.icon' :buttonText='card.button')
+  hr(width="80%" align=center)
+  div More stuff
+</template>
+
+<style lang="scss">
+$primary: #406275;
+$light: #DEE3E6;
+$white: tint-color($light, 70);
+
+.center-row {
+  background: linear-gradient(0deg, $white 50%, $primary 50%);
+}
+</style>
+
+<script>
+import card from './components/card'
+
+export default { 
+  components: {
+    card
+  },
+  
+  data() {
+    return {
+      cards: [
+        { id: 1, title: 'Blog', icon: 'fas fa-feather', excerpt: 'Thoughts on the current state of things', button: 'Read on'  },
+        { id: 2, title: 'Current Projects', icon: 'fas fa-pencil-ruler', excerpt: 'Things I\'m working on now', button: 'Dive in' },
+        { id: 3, title: 'Past Projects', icon: 'fas fa-archive', excerpt: 'Old or abandoned projects', button: 'Dig Deep' }
+      ]
+    }
+  }
+
+}
+</script>
