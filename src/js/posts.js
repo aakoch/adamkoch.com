2cb6e90bb888459b3122dbe391ca4c9103417e01
Adam Koch <adam@dogtired.org>
2021-08-26 00:47:11 -0500

    navbar fixes

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import App from "../components/posts";
-// import App from "../WIP";
-import 'bootstrap'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 const landing = createApp(App)
