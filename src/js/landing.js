96a9065a9bc8a672d155afc4afe9c1695ee505cf
Adam Koch <adam@dogtired.org>
2021-08-29 20:45:37 -0500

    remove unused comment and log

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,9 +1,7 @@
 import { createApp } from "vue";
 import App from "../components/landing";
-// import App from "../WIP";
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/css/all.css'
-console.log('process.env.NODE_ENV=' + process.env.NODE_ENV)
 
 const landing = createApp(App)
 landing.mount('#landing-app')
