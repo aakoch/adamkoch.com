7d3bad695886d1eddf8371836172808a291c2063
Adam Koch <adam@dogtired.org>
2021-10-09 09:33:20 -0500

    moved meta to own folder so I can copy it all

--- tests/system/basic.js
+++ tests/system/basic.js
@@ -1,7 +1,7 @@
 module.exports = {
   'homepage test': function (browser) {
     browser
-      .url('https://615512435071d1000785d98d--adamkoch.netlify.app/')
+      .url('https://6161a32f1a2daa0008148354--adamkoch.netlify.app/')
       .waitForElementVisible('body')
   },
   'Verify blog posts feather can be clicked': function (browser) {
