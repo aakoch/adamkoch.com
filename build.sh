8b984f013d187e60119512d585e357fa897be44b
Adam Koch <adam@dogtired.org>
2022-07-05 22:13:29 -0500

    change interpreter to bash; revert ls addition

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/bash
 mkdir -p dist && cp -v _redirects dist/ && cp -Rv src/assets/* dist/ && cp -Rv src/meta/* dist/
 for f in $(find src/posts/ -name "index.pug" | sort); do echo $f | sed -E "s/.\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/index.pug/\1-\2-\3/" >> posts.tmp; done; tail -n 2 posts.tmp | head -n 1 | xargs -I {} echo "{\"latestPost\": \"{}\"}" > blog.json
 node postpreviews.mjs
