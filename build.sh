ae7f48f04c3125ff472e0d68562cf154f57bad2d
Adam Koch <github2@adamkoch.com>
2023-02-07 20:09:16 -0600

    fix front page link

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 mkdir -p dist
 # cp -v _redirects dist/ && cp -v _headers dist/ && cp -Rv src/assets/* dist/ && cp -Rv src/meta/* dist/ && cp -Rvn src/img/* dist/
+for f in $(find src/app/blog/ -name "post.component.pug" | sort); do echo $f | sed -E "s/.*\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/post\/post.component.pug/\1-\2-\3/" >> posts.tmp; done; tail -n 1 posts.tmp | head -n 1 | xargs -I {} echo "{\"latestPost\": \"{}\"}" > blog.json
 ng build
 rm posts.tmp
-for f in $(find src/app/blog/ -name "post.component.pug" | sort); do echo $f | sed -E "s/.*\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/post\/post.component.pug/\1-\2-\3/" >> posts.tmp; done; tail -n 1 posts.tmp | head -n 1 | xargs -I {} echo "{\"latestPost\": \"{}\"}" > blog.json
 node postpreviews.mjs
 parcel build './src/assets/**/index.pug' './src/animations/*.pug' './src/rules/*.pug' --log-level verbose --dist-dir dist/assets --public-url "/assets"
