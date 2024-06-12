b41eed20226681ac7a71c32a2e60b16da03fb1f3
Adam Koch <github2@adamkoch.com>
2024-06-11 23:43:50 -0500

    updating build
    Signed-off-by: aakoch <github2@adamkoch.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir -p dist
-# cp -v _redirects dist/ && cp -v _headers dist/ && cp -Rv src/assets/* dist/ && cp -Rv src/meta/* dist/ && cp -Rvn src/img/* dist/
+cp -v src/_redirects dist/ && cp -v src/_headers dist/ && cp -Rv src/assets/* dist/ && cp -Rv src/meta/* dist/ && cp -Rvn src/img/* dist/
 for f in $(find src/app/blog/ -name "post.component.pug" | sort); do echo $f | sed -E "s/.*\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/post\/post.component.pug/\1-\2-\3/" >> posts.tmp; done; tail -n 1 posts.tmp | head -n 1 | xargs -I {} echo "{\"latestPost\": \"{}\"}" > blog.json
 ng build
 rm posts.tmp
